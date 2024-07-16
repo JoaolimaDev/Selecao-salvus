fe4a5ec235d5662843b94a74da28147d3154ee5c
JoaolimaDev <ozymandiasphp@gmail.com>
2024-07-16 00:11:11 -0300

    Primeiro Commit

---
+++ models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // Define associations here, e.g.:
+      // User.hasMany(models.Post);
+    }
+
+    static async hashPassword(password) {
+      return await bcrypt.hash(password, 10);
+    }
+
+    async validPassword(password) {
+      return await bcrypt.compare(password, this.password);
+    }
+  }
+
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await User.hashPassword(user.password);
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          user.password = await User.hashPassword(user.password);
+        }
+      },
+    },
+  });
+
+  return User;
+};
